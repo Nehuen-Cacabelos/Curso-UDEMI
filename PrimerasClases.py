8c6cbd0d70b94e23e287711e916613ca3713aca1
Nehuen <riveronehuen400@gmail.com>
2026-01-28 16:52:00 -0300

    first commit

---
+++ PrimerasClases.py
@@ -0,0 +1,10 @@
+"""print("Hola, Mundo!" + "\n" + "Bienvenidos a \"Python\".")
+print("\tEsta es una tercera linea tabulada.")
+
+print('This isn\'t a backslash: /')
+print("This isn't a backslash: \\")
+
+print("Tu nombre es "+ input("Escribe tu nombre: ") + " y tienes " + input("Ingresa tu edad: ") + " años.")
+"""
+
+print("La cerveza se llama: " + "\n" + "'" + input("Escribe el nombre de tu SuperHeroe favorito: ") + " "+ input(" Escribe tambien tu color favorito: ") + "'")
